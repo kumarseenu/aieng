a3e12b466b96a963fd64ad71bc143ec2bbcaec07
Vinoo Ganesh <vinooganesh@users.noreply.github.com>
2025-03-30 14:58:38 +0000

    try new installation

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,4 +1,16 @@
+# if [ -f requirements.txt ]; then
+#   pip install --upgrade pip
+#   pip install uv
+#   pip install --system -r requirements.txt
+# fi
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Add uv to PATH (if needed)
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# If requirements.txt exists, install dependencies with uv
 if [ -f requirements.txt ]; then
-  pip install uv
-  pip install --system -r requirements.txt
+  uv pip install -r requirements.txt
 fi
